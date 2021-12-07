bcc133f1606f2b48a5fbfb15dfc9dd64d722e99c
Tapan Kumer Das <voboghure.dev@gmail.com>
2021-12-07 18:24:32 +0600

    Add slides block for image and caption
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/blocks/slides/edit.js
+++ src/blocks/slides/edit.js
@@ -1,13 +1,64 @@
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	MediaUpload,
+	useBlockProps,
+} from "@wordpress/block-editor";
+import { Panel, PanelBody, TextControl, Button } from "@wordpress/components";
 
-import './editor.scss';
+import "./editor.scss";
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { imageCaption, imageSlide } = attributes;
+	const ALLOWED_MEDIA_TYPES = ["image"];
+
 	return (
-		<p {...useBlockProps()}>
-			{__('Sp Block Slider – hello from the editor!', 'sp-blocks')}
-		</p>
+		<>
+			<InspectorControls>
+				<Panel>
+					<PanelBody
+						title={__("Slider Image Caption", "sp-blocks")}
+						icon="format-status"
+					>
+						<TextControl
+							label={__("Image Caption", "sp-block")}
+							help={__("Write your slider image caption.", "sp-blocks")}
+							onChange={(imageCaption) => setAttributes({ imageCaption })}
+							value={imageCaption}
+						/>
+					</PanelBody>
+
+					<PanelBody
+						title={__("Slider Image Settings", "sp-blocks")}
+						icon="format-gallery"
+					>
+						<MediaUpload
+							onSelect={(imageSlide) =>
+								setAttributes({ imageSlide: imageSlide.sizes.full.url })
+							}
+							allowedTypes={ALLOWED_MEDIA_TYPES}
+							value={imageSlide}
+							render={({ open }) => (
+								<Button
+									onClick={open}
+									icon="format-image"
+									className="editor-media-placeholder__button is-button is-default is-large"
+								>
+									Open Media Library
+								</Button>
+							)}
+						/>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+
+			<div {...useBlockProps()}>
+				<div className="slide-image">
+					<img src={imageSlide} />
+				</div>
+				<div className="slide-caption">{imageCaption}</div>
+			</div>
+		</>
 	);
 }
--- src/index.js
+++
@@ -1,3 +0,0 @@
-// import each block
-import './blocks/slider'
-import './blocks/slides'
